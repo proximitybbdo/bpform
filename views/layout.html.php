778f9c89f0306b234470e5f0a62e3f6ee025730b
Jeroen Bourgois <jeroenbourgois@gmail.com>
2011-08-25 17:07:21 +0200

    small updates and finetuning

--- views/layout.html.php
+++ views/layout.html.php
@@ -5,6 +5,8 @@
 
   <title>Banner Platform</title>
 
+	<link rel="shortcut icon" href="<?php echo($base_path); ?>assets/img/favicon.png">
+
   <link rel="stylesheet" href="http://twitter.github.com/bootstrap/assets/css/bootstrap-1.1.0.min.css">
   <link rel="stylesheet" type="text/css" href="<?php echo($base_path); ?>assets/css/style.css">
 
